e5c99d5155fa95a8e1867c9badf762c6b9de2d21
nichenqin <nichenqin@gmail.com>
2018-10-09 17:19:46 +0800

    :bug: wrong api

--- packages/seact/src/utils/index.js
+++ packages/seact/src/utils/index.js
@@ -2,5 +2,5 @@ export function createShape(rect) {
   if (typeof MSShapeGroup.shapeWithPath === 'function') {
     return MSShapeGroup.shapeWithPath(rect)
   }
-  return MSShapeGroup.layerWithShape(rect)
+  return MSShapeGroup.layerWithPath(rect)
 }
